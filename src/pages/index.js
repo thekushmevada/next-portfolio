125fbbd9f7325da980e9db3cb2de25846b490931
thekushmevada <khmevada2003@gmail.com>
2023-11-05 11:34:51 +0530

    articles page removed from navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
-                <Link href="mailto:khmevada2003@gmail.com" target={"_blank"} className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base ">
+                <Link href="mailto:khmevada2003@gmail.com" target={"_blank"} className="hover:text-primary dark:hover:text-primaryDark ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base ">
                   Contact
                 </Link>
               </div>
